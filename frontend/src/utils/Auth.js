c57ec6a0baef276ce1fc920ecfa4c806583781cc
Amyrga Tavakai <tavakaiam@yandex.ru>
2021-06-23 20:49:02 +0300

    true req url

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://mesto.tavakai.nomoredomains.monster';
 
 export const register = (data) => {
   return fetch(`${BASE_URL}/signup`, {
